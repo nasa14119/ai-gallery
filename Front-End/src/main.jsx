291337d31a668626c05867f8d72c6a627c178e89
nasa14119 <nico.amaya0205@gmail.com>
2023-07-28 16:06:55 -0600

    fix !(root) : hash router insted of browser router implemented

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<BrowserRouter><App /></BrowserRouter>)
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <HashRouter>
+    <App/>
+  </HashRouter>
+);
