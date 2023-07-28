9b9d72e1ee37c893d1a4db5fd74cf4309498a468
nasa14119 <nico.amaya0205@gmail.com>
2023-07-27 19:34:49 -0600

    fix(app): cors try fix

--- Back-End/app.js
+++ Back-End/app.js
@@ -3,12 +3,12 @@ import auth from "./routes/auth.routes.js"
 import images from "./routes/images.routes.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"; 
-import imagesModel from "./models/images.model.js";
+
 const app = express(); 
 
 //configs of express
 app.use(express.json()); 
-app.use(cors({credentials: true, origin:"http://localhost:3000"})); 
+app.use(cors({credentials: true, origin : (orgin, callback) => callback()})); 
 app.use(cookieParser()); 
 app.get("/", (req, res) =>{
   res.send("Welcome to the api for the app of images"); 
