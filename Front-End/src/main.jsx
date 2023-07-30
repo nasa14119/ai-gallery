d2976222dad7e885a5e4302862cbfca1d65879e4
nasa14119 <nico.amaya0205@gmail.com>
2023-07-30 14:25:57 -0600

    feat(main): switching back to browser router, exiting error fixed in server

--- Front-End/src/main.jsx
+++ Front-End/src/main.jsx
@@ -1,12 +1,12 @@
 import './index.css'
 import App from './App.jsx';
 import { createRoot } from "react-dom/client";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App/>
-  </HashRouter>
+  </BrowserRouter>
 );
