d34c5dd6e4da1b4dbcefd1c3ac3749ec58e7651b
nasa14119 <nico.amaya0205@gmail.com>
2023-07-31 16:37:12 -0600

    change maked

--- Back-End/controllers/auth.controller.js
+++ Back-End/controllers/auth.controller.js
@@ -9,7 +9,7 @@ const COOKIE_OPTIONS = {
   sameSite: "none",
   secure: "true",
   httpOnly:false, 
-  domain:"fullstack-gallery.onrender.com"
+  domain:"mygalleryofimages.onrender.com"
 }
 
 export const login = async (req, res) => {
