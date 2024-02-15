628652b133dced7a51a4f431fe72290231ad3ee8
nasa14119 <nico.amaya0205@gmail.com>
2024-02-15 14:31:34 -0600

    deleting comment
    Signed-off-by: nasa14119 <nico.amaya0205@gmail.com>

--- Front-End/src/pages/Dashboard/components/MainImages.jsx
+++ Front-End/src/pages/Dashboard/components/MainImages.jsx
@@ -12,4 +12,3 @@ export default function MainImages() {
   }
   return <Gallery sources={data} />;
 }
-// https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80
