5f28ec76ad87dd754b1e17580d386a7a4ba3e6cd
nasa14119 <nico.amaya0205@gmail.com>
2024-08-01 11:15:03 -0600

    feat(AuthSection): Adding component for auth only content

---
+++ Front-End/src/pages/Home/AuthSection/index.jsx
@@ -0,0 +1,17 @@
+import "./styles.css"
+import { useAuth } from '../../../context/auth.context';
+import { Nav } from "../Nav"
+
+export function AuthSection() {
+    const { user } = useAuth()
+  return (
+    <main className="h-screen w-full flex flex-col justify-center items-center">
+      <Nav />
+      <h1 className="text-3xl md:text-5xl text-center select-none pointer-events-none">
+        <strong className="text-5xl md:text-7xl">Welcome</strong>
+        <br />
+        <span>{user.username}</span>
+      </h1>
+    </main>
+  );
+}
--- Front-End/src/pages/Home/index.jsx
+++ Front-End/src/pages/Home/index.jsx
@@ -3,28 +3,10 @@ import { Button } from "./Button";
 import { FollowMouse } from "../../components/FollowMouse";
 import "./styles.css"
 import { useAuth } from "../../context/auth.context";
-import { Link } from "react-router-dom";
-import { Nav } from "./Nav";
+import { AuthSection } from "./AuthSection";
 function Home() {
-  const { isAuth, user} = useAuth();
-  if (isAuth) {
-    return (
-      <main className="h-screen w-full flex flex-col justify-center items-center">
-        <Nav/>
-        <h1 className="text-3xl md:text-5xl text-center select-none pointer-events-none">
-          <strong className="text-5xl md:text-7xl">Welcome</strong>
-          <br />
-          <span>{user.username}</span>
-        </h1>
-        <div className="h-20 flex items-center justify-center mt-10 w-3/4 gap-5">
-          <Link to="/dashboard" className="text-white text-center text-[28px] px-3 py-2 bg-gradient-to-br from-purple-600 to-blue-500 rounded-xl cursor-pointer font-medium transition-all duration-700 hover:scale-110">
-            Go to Dashboard
-          </Link>
-        </div>
-        <FollowMouse />
-      </main>
-    );
-  }
+  const { isAuth} = useAuth();
+  if (isAuth) return <AuthSection />
   return (
     <main className="h-screen w-full flex flex-col justify-center items-center">
       <SwitchTheme />
