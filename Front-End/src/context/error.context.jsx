96452e40b207c907ab24091c187154eb5cdc297b
nasa14119 <nico.amaya0205@gmail.com>
2024-08-04 12:52:24 -0600

    adding error context

---
+++ Front-End/src/context/error.context.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+import { useMakeError } from "../hooks/useMakeError";
+
+const ErrorContext = createContext(null); 
+
+export const ErrorProvider = ({children}) => {
+    const [Error, addError] = useMakeError()
+    return (
+        <ErrorContext.Provider values={{Error, addError}}>
+            {children}
+        </ErrorContext.Provider>
+    )
+}
+export const useTriggerError = () => {
+    const addError = useContext(ErrorContext).addError
+    return (message) => addError(message)
+}; 
+
+export const useErrorComponent = () => useContext(ErrorContext).Error
