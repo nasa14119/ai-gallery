295401aaa598d14a305b8314a84e9575c8d1905a
nasa14119 <nico.amaya0205@gmail.com>
2024-08-18 20:22:24 -0600

    stash command saved

--- Back-End/utils/cloudflare.js
+++ Back-End/utils/cloudflare.js
@@ -1,18 +1,22 @@
-import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
 import { readFile } from "node:fs/promises";
 import { __dirname } from "./fs.js";
 
 export class CloudFlare {
   constructor(options) {
     const credentials = {
-      accessKeyId: options.accessKeyId, 
-      secretAccessKey: options.secretAccessKey
-    }
-    console.log(credentials)
+      accessKeyId: options.accessKeyId,
+      secretAccessKey: options.secretAccessKey,
+    };
+    console.log(credentials);
     this.client = new S3Client({
       region: "auto",
-      credentials, 
-      endpoint: options.endpoint
+      credentials,
+      endpoint: options.endpoint,
     });
     this.bucket = options.bucket_name ?? "ai-images";
   }
@@ -31,8 +35,8 @@ export class CloudFlare {
     const params = {
       Bucket: this.bucket,
       Key: hash,
-    }
-    const comand = new GetObjectCommand(params); 
+    };
+    const comand = new GetObjectCommand(params);
     try {
       const res = await this.client.send(comand);
       return res.Body;
--- Back-End/utils/fs.js
+++ Back-End/utils/fs.js
@@ -6,38 +6,38 @@ import { existsSync } from "node:fs";
 export const __dirname = path.resolve();
 
 export const isFileCached = async (hash) => {
-  const img_directory = path.join(__dirname, "Back-End", "img"); 
-  if(!existsSync(img_directory)) {
-    await mkdir(img_directory, {recursive: true}); 
-    return false
+  const img_directory = path.join(__dirname, "Back-End", "img");
+  if (!existsSync(img_directory)) {
+    await mkdir(img_directory, { recursive: true });
+    return false;
   }
-  return existsSync(path.join(img_directory, hash + ".png",))
-}
+  return existsSync(path.join(img_directory, hash + ".png"));
+};
 
 export const getHashFile = async (hash) => {
-  if(await isFileCached(hash)){
-    return path.join(__dirname, "Back-End", "img", hash + ".png")
+  if (await isFileCached(hash)) {
+    return path.join(__dirname, "Back-End", "img", hash + ".png");
   }
-  throw new Error("File not found in cache"); 
+  throw new Error("File not found in cache");
 };
 
 export const getHashPath = (hash) => {
-  return path.join(__dirname, "/Back-End/img", hash+".png");
-}
+  return path.join(__dirname, "/Back-End/img", hash + ".png");
+};
 
 export const saveImgBase64 = async (base64Data, hash) => {
   let error = null;
   try {
-    if(!await isFileCached(hash)){
-      const dir = getHashPath(hash); 
+    if (!(await isFileCached(hash))) {
+      const dir = getHashPath(hash);
       await writeFile(dir, base64Data, "base64", (error) => {
         console.log(error);
         error = "Something went wrong saving the file";
       });
     }
-    throw new Error("File is cached"); 
+    throw new Error("File is cached");
   } catch (err) {
-    error = err
+    error = err;
   }
   return [error, hash];
 };
@@ -47,14 +47,13 @@ export const getHash = () => crypto.randomBytes(10).toString("hex");
 export const removeCache = async (hash) => {
   let error = null;
   try {
-    const directory = await getHashFile(hash)
+    const directory = await getHashFile(hash);
     await unlink(directory, (e) => {
       console.log(e);
       error = "Somenthing went wrong deleting file";
     });
   } catch (err) {
-    error = err
+    error = err;
   }
   return error;
 };
-
